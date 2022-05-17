9b1adaff6a860e0f3e258933abde017c4b9b9026
cit <cit@MacBook-Pro-de-cit.local>
2022-05-17 13:43:34 -0300

    Setup Projeto

---
+++ challengeOne/challengeOne/SwiftUIView.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftUIView.swift
+//  challengeOne
+//
+//  Created by cit on 17/05/22.
+//
+
+import SwiftUI
+import SwiftDate
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
---
+++ challengeOne/challengeOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  challengeOne
+//
+//  Created by cit on 17/05/22.
+//
+
+import UIKit
+import SwiftDate
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
